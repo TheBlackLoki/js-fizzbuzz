f00675d3f4e96e9a01ab889817d40e29fcc01dcb
Luca Stefani <theblackloki@gmail.com>
2020-03-26 16:30:21 +0100

    stampa lista su schermo versione compatta

--- main2.js
+++ main2.js
@@ -1,15 +1,20 @@
-
-
+var lista = []
 
 for (var numero = 1; numero <= 100; numero ++) {
 
   if (numero % 3 == 0 && numero % 5 == 0) {
-    console.log("FizzBuzz");
+    lista.push("FizzBuzz");
   }else if (numero % 5 == 0) {
-    console.log("Buzz")
+    lista.push("Buzz")
   }else if (numero % 3 == 0) {
-    console.log("Fizz");
+    lista.push("Fizz");
   }else {
-    console.log(numero);
+    lista.push(numero);
   }
+}
+console.log(lista);
+var schermo = "<li>1</li>"
+for (var i = 1; i < lista.length; i++) {
+  schermo = schermo + "<li>" + lista[i] + "</li>"
+  document.getElementById("mioID").innerHTML = schermo
 }
